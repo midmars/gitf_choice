4cb1658b784810b774b0a1244f60fdb13bd71a74
midmars <rickychen0711@gmail.com>
2017-04-27 16:57:50 +0800

    Revert "upload gift_choice"
    This reverts commit 4d47d9e255ee1b268f9520d96d6fe03658b4e70f.

--- htman/school/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Member(models.Model):
-	member_pk = models.AutoField(max_length=255,primary_key=True)
-	name = models.CharField(max_length=255)
-	status = models.CharField(max_length=255)
-	weight = models.IntegerField()
-	member_img= models.CharField(max_length=255,null=True)
